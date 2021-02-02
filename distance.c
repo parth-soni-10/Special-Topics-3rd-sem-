c7a990aa3bab93e34e25a3384544ebc385ea0bd3
the-imperfect-kid <76111886+the-imperfect-kid@users.noreply.github.com>
2021-02-02 18:19:39 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,1 +1,47 @@
 //WAP to find the distance between two point using 4 functions.
+#include <stdio.h>
+#include <math.h>
+float xone();
+float xtwo();
+float yone();
+float ytwo();
+int main()
+{
+    float gdistance;
+	float a=xone();
+	float b=xtwo();
+	float c=yone();
+	float d=ytwo();
+	gdistance = ((b-a)*(b-a))+((d-c)*(d-c));
+	printf("Distance between the said points: %.4f", sqrt(gdistance));
+	printf("\n");
+	return 0;
+}
+float xone()
+{
+    float x1;
+    printf("Input x1: ");
+	scanf("%f", &x1);
+	return x1;
+}
+float xtwo()
+{
+    float x2;
+    printf("Input x2: ");
+    scanf("%f", &x2);
+	return x2;
+}
+float yone()
+{
+    float y1;
+    printf("Input y1: ");
+	scanf("%f", &y1);
+    return y1;
+}
+float ytwo()
+{
+    float y2;
+    printf("Input y2: ");
+	scanf("%f", &y2);
+	return y2;
+}
