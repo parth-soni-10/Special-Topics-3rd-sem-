fdfa41ccb4bb4224a253140a474b6f01e309ac0a
the-imperfect-kid <76111886+the-imperfect-kid@users.noreply.github.com>
2021-02-08 22:08:43 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,32 @@
 //WAP to find the distance between two points using structures and 4 functions.
+#include <math.h>
+struct dis
+{
+    float x,y;
+}one,two;
+int distance()
+{
+    float total=((one.y-one.x)*(one.y-one.x))+((two.y-two.x)*(two.y-two.x));
+    total = sqrt(total);
+    printf("The distance between the points is:-- %f",total);
+}
+int input1()
+{
+    printf("Enter the values x coordinate of point 1:--");
+    scanf("%f",&one.x);
+    printf("Enter the values y coordinate of point 1:--");
+    scanf("%f",&one.y);
+}
+int input2()
+{
+    printf("Enter the values x coordinate of point 2:--");
+    scanf("%f",&two.x);
+    printf("Enter the values y coordinate of point 2:--");
+    scanf("%f",&two.y);
+}
+int main()
+{
+    input1();
+    input2();
+    distance();
+}
