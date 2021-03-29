4f73361c19fb2e25300e4ee5bfe624f064bad150
the-imperfect-kid <76111886+the-imperfect-kid@users.noreply.github.com>
2021-03-29 11:19:16 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,50 +1,72 @@
-//WAP to find the sum of n fractions.
-#include <stdio.h>
-struct fracs
+#include<stdio.h>
+typedef struct frac
 {
-    int num,den;
-}f;
+	int n,d;
+}frac;
+frac input_one()
+{
+	frac f;
+printf("Enter the numerator and denominator:- ");
+scanf("%d%d",&f.n,&f.d);
+return f;
+}
+int nof()
+{
+	int n;
+	printf("Enter the no of fractions :-");
+	scanf("%d",&n);
+	return n;
+}
+void input_n(int n , frac arr[n])
+{
+	for(int i = 0;i<n,i++)
+		a[i]=input_one();
+}
+int gcd_calc(int x,int  y)
+{
+    int gcd =1;
+    for (int i = 2; i<=x && i<=y;i++)
+    {
+        if (x%i==0 && y%i==0)
+            gcd = i;
+    }
+    return gcd;
+}
+frac add(frac x, frac y)
+{
+    frac sum;
+    sum.num = (x.num*y.denom)+(y.num*x.denom);
+    sum.denom= x.denom*y.denom;
+    int h=gcd_calc(sum.num,sum.denom);
+    sum.num=sum.num/h;
+    sum.denom=sum.denom/h;
+ return sum;
+}
+frac add_n(int n, frac arr[n])
+{
+	frac temp;
+	temp.n=0;
+	temp.d=1;
+	for(int i =0;i<n;i++)
+	{
+	temp = add(temp,arr[i]);
+}
+return temp;
+}
+int output(int n , frac b[n], frac sum)
+{
+	printf("The sum of fractions : - ");
+	for(int i=0;i<n;i++)
+	{
+	printf("%d / %d",b[i].n,b[i].d);
+}
+printf("The sum is : - %d / %d " sum.n,sum.d);
+}
 int main()
 {
-    struct fracs a[100];
-    int n;
-    printf("Enter the number of fractions:- ");
-    scanf("%d",&n);
-    input(a,n);
-    addition(a,n);
-}
-int input(struct fracs a[], int n)
-{
-    for(int i=0;i<n;i++)
-    {
-        printf("Enter the numerator for fraction %d :- ",i+1);
-        scanf("%d",&a[i].num);
-        printf("Enter the denominator for fraction %d :-    ",i+1);
-        scanf("%d",&a[i].den);
-    }
-}
-int addition(struct fracs a[], int n)
-{
-    struct fracs sum;
-    sum.num = 0;
-    sum.den = a[0].den;
-    for(int i=0;i<n;i++)
-    {
-        sum.den = ((a[i].den*sum.den)/gcd(a[i].den,sum.den));
-    }
-    for(int i=0;i<n;i++)
-    {
-        sum.num = sum.num + (a[i].num)*(sum.den/a[i].den);
-    }
-    int hcf = gcd(sum.num,sum.den);
-    sum.num = sum.num/hcf;
-    sum.den = sum.den/hcf;
-    printf("The sum of the fractions is :- %d/%d",sum.num,sum.den);
-}
-int gcd(int a, int b)
-{
-    if(a==0)
-        return b;
-    else
-        return gcd(b%a,a);
+	int nf= nof();	
+	frac noff[nf];
+	input_n(nf,noff);
+	frac f_sum=  add_n(nf,noff);
+	output(nf,noff,f_sum);
 }
